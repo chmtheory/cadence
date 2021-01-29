0b92eb53638597109c6fc63d761b8a4aa16f0eb0
chmtheory <j97k@protonmail.com>
2021-01-29 01:42:23 -0700

    Login

--- src/main/java/control/CadenceMain.java
+++ src/main/java/control/CadenceMain.java
@@ -1,9 +1,44 @@
 package control;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CadenceMain {
+
+    private final String botToken;
+    private final static Logger log = LoggerFactory.getLogger(CadenceMain.class);
 
     public static void main(String[] args) {
 
+        try {
+            String token = getToken();
+            CadenceMain main = new CadenceMain(token);
+            main.start();
+        } catch (Exception ex) {
+            log.error("Unexpected exception while initializing!", ex);
+        }
+
+    }
+
+    public CadenceMain(String token) {
+        botToken = token;
+    }
+
+    public void start() throws LoginException {
+        JDA jda = JDABuilder.createDefault(botToken).build();
+        jda.addEventListener(new CadenceListener());
+    }
+
+    private static String getToken() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("token"));
+        return reader.readLine();
     }
 
 }
