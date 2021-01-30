125735a68d5b8a735fc9b57935d2dbebb34ba42a
chmtheory <j97k@protonmail.com>
2021-01-30 02:25:57 -0700

    Minor functionality implementation - will probably go back on this

--- src/main/java/util/DiscordUtil.java
+++ src/main/java/util/DiscordUtil.java
@@ -1,9 +1,29 @@
 package util;
 
-public class DiscordUtil {
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.function.Consumer;
+
+public final class DiscordUtil {
 
     private DiscordUtil() {
 
+    }
+
+    public static void sendMessage(GuildMessageReceivedEvent event, String message) {
+        event.getChannel().sendMessage(message).queue();
+    }
+
+    public static void sendMessage(TextChannel channel, String message) {
+        channel.sendMessage(message).queue();
+    }
+
+    public static TextChannel getDefaultTextChannel(Guild guild) {
+        return guild.getDefaultChannel();
     }
 
 }
