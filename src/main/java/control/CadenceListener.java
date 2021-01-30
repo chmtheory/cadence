e642bde052ee5d2924d106211dccd0d7892e7157
chmtheory <j97k@protonmail.com>
2021-01-30 02:36:58 -0700

    Basic command functionality

--- src/main/java/control/CadenceListener.java
+++ src/main/java/control/CadenceListener.java
@@ -1,15 +1,18 @@
 package control;
 
 import command.Command;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import util.DiscordUtil;
 
 import java.util.HashMap;
 
 public class CadenceListener extends ListenerAdapter {
 
     private final HashMap<String, Command> commandMap;
+    private final String commandPrefix = "!";
 
     public CadenceListener(HashMap<String, Command> commandMap) {
         this.commandMap = commandMap;
@@ -17,6 +20,22 @@ public class CadenceListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
-        super.onGuildMessageReceived(event);
+        User user = event.getAuthor();
+
+        if (user.isBot()) {
+            return; // Never listen to bots!
+        }
+
+        String first = DiscordUtil.firstWord(event.getMessage());
+
+        if (first.startsWith(commandPrefix)) {
+            first = first.substring(1);
+
+            Command c;
+            if ((c = commandMap.get(first)) != null) {
+                c.execute(event);
+            }
+        }
     }
+
 }
