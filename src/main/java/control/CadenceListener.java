b72facc61f67bf3383ee71db0f5e868a9cbf38a9
chmtheory <j97k@protonmail.com>
2021-01-30 17:43:35 -0700

    Implemented onReady and onGuildJoin

--- src/main/java/control/CadenceListener.java
+++ src/main/java/control/CadenceListener.java
@@ -1,21 +1,47 @@
 package control;
 
 import command.Command;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.DiscordUtil;
 
 import java.util.HashMap;
 
 public class CadenceListener extends ListenerAdapter {
 
+    private final static Logger log = LoggerFactory.getLogger(CadenceListener.class);
+    private final HashMap<Long, Server> serverMap = new HashMap<>();
     private final HashMap<String, Command> commandMap;
     private final String commandPrefix = "!";
 
     public CadenceListener(HashMap<String, Command> commandMap) {
         this.commandMap = commandMap;
+    }
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        JDA jda = event.getJDA();
+        jda.getGuildCache().forEachUnordered(guild -> {
+            serverMap.put(guild.getIdLong(), new Server(guild));
+        });
+
+        super.onReady(event);
+    }
+
+    @Override
+    public void onGuildJoin(@NotNull GuildJoinEvent event) {
+        Guild guild = event.getGuild();
+        serverMap.put(guild.getIdLong(), new Server(guild));
+
+        super.onGuildJoin(event);
     }
 
     @Override
