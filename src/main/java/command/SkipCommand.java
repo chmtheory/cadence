96491383aad1f42360088fc954cf84983623a106
chmtheory <j97k@protonmail.com>
2021-01-30 18:29:24 -0700

    Implemented basic functionality

--- src/main/java/command/SkipCommand.java
+++ src/main/java/command/SkipCommand.java
@@ -1,12 +1,22 @@
 package command;
 
 import control.Server;
+import music.PlayerState;
+import music.ServerPlayer;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import util.DiscordUtil;
 
 public class SkipCommand implements Command {
     @Override
     public void execute(GuildMessageReceivedEvent event, Server server) {
+        ServerPlayer player = server.getPlayer();
 
+        if (player.getState() != PlayerState.PLAYING) {
+            DiscordUtil.sendMessage(event, "Can't skip track when paused or stopped!");
+        } else {
+            player.skip();
+            DiscordUtil.sendMessage(event, "Skipping current track!");
+        }
     }
 
     @Override
