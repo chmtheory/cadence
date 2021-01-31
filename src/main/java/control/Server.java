3d62bc3586a3e4cd84c7a5200582311f33f9d824
chmtheory <j97k@protonmail.com>
2021-01-30 17:41:59 -0700

    Reorganized package

---
+++ src/main/java/control/Server.java
@@ -0,0 +1,40 @@
+package control;
+
+import music.ServerPlayer;
+import music.TrackInfo;
+import net.dv8tion.jda.api.entities.Guild;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server {
+
+    private final Guild guild;
+    private final ServerPlayer player;
+    private final ConcurrentHashMap<String, TrackInfo> tracks = new ConcurrentHashMap<>();
+
+    public Server(Guild guild) {
+        this.guild = guild;
+        this.player = new ServerPlayer(this);
+    }
+
+    public List<TrackInfo> getPlaylist() {
+        ArrayList<TrackInfo> playlist = new ArrayList<>();
+        playlist.addAll(tracks.values());
+        return playlist;
+    }
+
+    public void addTrack(String id, String name) {
+        tracks.put(id, new TrackInfo(id, name));
+    }
+
+    public void removeTrack(String id) {
+        tracks.remove(id);
+    }
+
+    public Guild getGuild() {
+        return guild;
+    }
+
+}
